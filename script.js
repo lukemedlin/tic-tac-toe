d4298d3479f6f93e412e4054fc7a5436d5bdcac6
lukemedlin <41134833+lukemedlin@users.noreply.github.com>
2023-02-06 05:06:42 -0600

    Create initial game board html and logic

---
+++ script.js
@@ -0,0 +1,34 @@
+const gameBoard = (() => {
+  const board = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+  ];
+
+  const gameBoardDiv = document.querySelector('.game-grid');
+  const gameSpotDivs = document.querySelectorAll('.game-spot');
+
+  const getValue = (x, y) => {
+    return board[x][y];
+  };
+
+  const setValue = (x, y, player) => {
+    board[x][y] = player;
+  };
+
+  const render = () => {
+    let child = 0;
+    for (let row = 0; row < board[0].length; row += 1) {
+      for (let col = 0; col < board.length; col += 1 ) {
+        gameSpotDivs[child].textContent = board[row][col];
+        child += 1;
+      }
+    }
+  };
+
+  return {
+    getValue,
+    setValue,
+    render
+  };
+})();
