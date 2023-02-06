0f60428c6f0aa9725c8e82b0cf90f7a8b59c2d96
lukemedlin <41134833+lukemedlin@users.noreply.github.com>
2023-02-06 05:50:35 -0600

    Add ability to mark Os on the board

--- script.js
+++ script.js
@@ -16,10 +16,17 @@ const gameBoard = (() => {
     board[x][y] = player;
   };
 
+  const markSpot = (event) => {
+    board[event.currentTarget.dataset.row][event.currentTarget.dataset.col] = 'O'
+    render();
+  }
+
+  gameSpotDivs.forEach((div) => div.addEventListener('click', markSpot));
+
   const render = () => {
     let child = 0;
     for (let row = 0; row < board[0].length; row += 1) {
-      for (let col = 0; col < board.length; col += 1 ) {
+      for (let col = 0; col < board.length; col += 1) {
         gameSpotDivs[child].textContent = board[row][col];
         child += 1;
       }
@@ -29,6 +36,6 @@ const gameBoard = (() => {
   return {
     getValue,
     setValue,
-    render
+    render,
   };
 })();
